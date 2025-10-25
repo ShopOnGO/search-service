6118773fd62787c09ddf10e5dfbfbae99233d19e
LordVillain <danyailyinchyk2005@gmail.com>
2025-10-25 16:36:24 +0300

    сделал health cheak

--- internal/elastic/client.go
+++ internal/elastic/client.go
@@ -1,37 +1,46 @@
 package elastic
 
 import (
+	"time"
+
 	"github.com/ShopOnGO/ShopOnGO/pkg/logger"
 	"github.com/ShopOnGO/search-service/configs"
 	"github.com/elastic/go-elasticsearch/v7"
 )
 
 var (
-    ESClient *elasticsearch.Client
-    Index    string
+	ESClient *elasticsearch.Client
+	Index    string
 )
 
 func Init(cfg *configs.Config) {
-    esCfg := elasticsearch.Config{
-        Addresses: []string{cfg.Elastic.URL},
-    }
+	esCfg := elasticsearch.Config{
+		Addresses: []string{cfg.Elastic.URL},
+	}
 
-    client, err := elasticsearch.NewClient(esCfg)
-    if err != nil {
-        logger.Errorf("[elastic] failed to create client: %v", err)
-        return
-    }
+	var client *elasticsearch.Client
+	var err error
 
-    // Проверка соединения
-    res, err := client.Info()
-    if err != nil {
-        logger.Errorf("[elastic] failed to connect to Elasticsearch: %v", err)
-        return
-    }
-    defer res.Body.Close()
+	// Retry loop
+	for i := 0; i < 10; i++ {
+		client, err = elasticsearch.NewClient(esCfg)
+		if err == nil {
+			res, err := client.Info()
+			if err == nil && res.StatusCode == 200 {
+				res.Body.Close()
+				break
+			}
+		}
+		logger.Warnf("[elastic] waiting for Elasticsearch... (%d/10)", i+1)
+		time.Sleep(2 * time.Second)
+	}
 
-    ESClient = client
-	logger.Info("Elasticsearch client from init:", ESClient)
-    Index = cfg.Elastic.Index
-    logger.Infof("[elastic] connected to %s | index=%s", cfg.Elastic.URL, Index)
+	if err != nil {
+		logger.Errorf("[elastic] failed to connect to Elasticsearch: %v", err)
+		return
+	}
+
+	ESClient = client
+	Index = cfg.Elastic.Index
+	logger.Infof("[elastic] connected to %s | index=%s", cfg.Elastic.URL, Index)
 }
